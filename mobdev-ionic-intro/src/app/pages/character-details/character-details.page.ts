a7fe058a32447945d007767f526a565a32626c6a
winniemdelgado <wm.delgado@hotmail.com>
2020-04-24 19:56:50 +0000

    update

--- mobdev-ionic-intro/src/app/pages/character-details/character-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/character-details/character-details.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: 'app-character-details',
-  templateUrl: './character-details.page.html',
-  styleUrls: ['./character-details.page.scss'],
+    selector: 'app-character-details',
+    templateUrl: './character-details.page.html',
+    styleUrls: ['./character-details.page.scss'],
 })
 export class CharacterDetailsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    character: any;
+    characterId = null;
+    constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+    ngOnInit() {
+        this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+        this.api.getCharacter(this.characterId).subscribe(res => {
+            this.character = res[0];
+        })
+    }
 }
--- mobdev-ionic-intro/src/app/services/api.service.ts
+++ mobdev-ionic-intro/src/app/services/api.service.ts
@@ -9,28 +9,28 @@ export class ApiService {
     constructor(private http: HttpClient) { }
 
     getEpisodes() {
-        return this.http.get(`https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/episodes.json`)
+        return this.http.get('https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/episodes.json')
     }
 
     getEpisode(id) {
-        return this.http.get(`https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/episode.json`);
+        return this.http.get('https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/episode.json');
     }
     getCharacters() {
-        return this.http.get(`https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/characters.json`)
+        return this.http.get('https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/characters.json')
     }
 
     getCharacter(char_id) {
-        return this.http.get(`https://www.breakingbadapi.com/api/characters/${char_id}`);
+        return this.http.get('https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/character.json');
     }
     getQuotes() {
-        return this.http.get(`https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/quotes.json`)
+        return this.http.get('https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/quotes.json')
     }
 
     getQuote(quote_id) {
-        return this.http.get(`https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/quote.json`);
+        return this.http.get('https://8100-e6fe03bb-b324-455b-8733-5f60e110241b.ws-eu01.gitpod.io/assets/quote.json');
     } 
     
     getDeaths() {
-        return this.http.get(`https://www.breakingbadapi.com/api/death-count?name=Gustavo+Fring`)
+        return this.http.get('https://www.breakingbadapi.com/api/death-count?name=Gustavo+Fring')
     }
 }
